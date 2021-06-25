fd75a2faf610e323fa61045aa7c9d1af4be0685a
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-06-25 12:06:01 +0530

    basic crud updation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/CampWithCare',{
     useNewUrlParser:true,
@@ -21,6 +22,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -41,10 +43,27 @@ app.post('/campgrounds',async(req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
+app.get('/campgrounds/:id/edit', async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+});
+
+app.put('/campgrounds/:id', async (req,res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`); 
+});
+
 app.get('/campgrounds/:id',async (req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground});
 });
+
+app.delete('/campgrounds/:id', async (req,res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 app.listen(3000,()=>{
     console.log('Serving on port 3000');
