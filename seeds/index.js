2b4b3f1b3a0689fa2b1fe763afbe15b62b9e40c6
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-06-26 13:33:07 +0530

    added some styles

--- seeds/index.js
+++ seeds/index.js
@@ -20,10 +20,14 @@ const sample = array => array[Math.floor(Math.random()*array.length)];
 const seedDB = async ()=>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){    
-        const random1000 = Math.floor(Math.random()*1000);
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title : `${sample(descriptors)} ${sample(places)}`,
-            location : `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image : 'https://source.unsplash.com/collection/483251',
+            description : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur nostrum commodi quas veritatis, odit quam molestiae perspiciatis tenetur illum? Voluptates delectus, nihil saepe deserunt sequi mollitia vel quasi corrupti odit!",
+            price  
         });
         await camp.save();
     }
