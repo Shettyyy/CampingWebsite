8a9a44b2951791fc790217927d2e76f29612ccf0
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-07-01 14:08:00 +0530

    added joi

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 
 mongoose.connect('mongodb://localhost:27017/CampWithCare',{
     useNewUrlParser:true,
@@ -43,7 +44,20 @@ app.get('/campgrounds/new',(req,res)=>{
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
+    const campgroundSchema = joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    }).required();
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(ele => ele.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
