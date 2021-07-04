8288ac3646d4290740a8c6017aa3b5a536704b91
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-07-04 12:53:36 +0530

    added sperate routes

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema, reviewSchema } = require('./schemas');
+const campground = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/CampWithCare',{
     useNewUrlParser:true,
     useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology: true,
+    useFindAndModify:false
 });
 
 const db = mongoose.connection;
@@ -25,90 +24,19 @@ const app = express();
 
 app.engine('ejs',ejsMate);
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(methodOverride('_method'));
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(ele => ele.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(ele => ele.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    else {
-        next();
-    }
-}
+app.use('/campgrounds', campground);
+app.use('/campgrounds/:id/reviews', reviews);
 
 app.get('/',(req,res)=>{
     res.render('home');
 });
-
-app.get('/campgrounds', catchAsync(async (req,res,next)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index',{campgrounds});
-}));
-
-app.get('/campgrounds/new',(req,res)=>{
-    res.render('campgrounds/new');
-});
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req,res,next)=>{
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit',{campground});
-}));
-
-app.get('/campgrounds/:id',catchAsync(async (req,res,next)=>{
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show',{campground});
-}));
-
-app.post('/campgrounds',validateCampground, catchAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.put('/campgrounds/:id', validateCampground,catchAsync(async (req,res,next) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`); 
-}));
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { review: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
