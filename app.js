2947f4dc6edec12567f29aa6d4376520cd28f6fd
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-07-04 15:00:21 +0530

    did some restructuring and added flash

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const campground = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -22,6 +24,17 @@ db.once("open",()=>{
 
 const app = express();
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly:true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
 app.engine('ejs',ejsMate);
 
 app.set('view engine', 'ejs');
@@ -30,6 +43,18 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sessionConfig));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/campgrounds', campground);
 app.use('/campgrounds/:id/reviews', reviews);
