e9b1c41d44a5e09af3a3bb222ba6103b4babf689
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-07-06 21:42:25 +0530

    added image upload feature

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_400,');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -12,10 +21,12 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref:'User'
     },
-    reviews: [{
-        type: Schema.Types.ObjectId,
-        ref:'Review'
-    }]
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,22 @@ const seedDB = async ()=>{
             author:'60e29127011dbd3b9874b81e',
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur nostrum commodi quas veritatis, odit quam molestiae perspiciatis tenetur illum? Voluptates delectus, nihil saepe deserunt sequi mollitia vel quasi corrupti odit!",
-            price  
+            price,
+            images: [
+                {
+                    url : "https://res.cloudinary.com/dilsqlwhn/image/upload/v1625579095/CampWithCare/zowop1spmuscqers74ld.jpg",
+                    filename : "CampWithCare/zowop1spmuscqers74ld"
+                },
+                {
+                    url : "https://res.cloudinary.com/dilsqlwhn/image/upload/v1625579101/CampWithCare/vzyiik3vpo5azyt3rohg.jpg",
+                    filename : "CampWithCare/vzyiik3vpo5azyt3rohg"
+                },
+                {
+                    url : "https://res.cloudinary.com/dilsqlwhn/image/upload/v1625579115/CampWithCare/edyytbamkno3nxjvztfm.jpg",
+                    filename : "CampWithCare/edyytbamkno3nxjvztfm"
+                }
+            ]
         });
         await camp.save();
     }
