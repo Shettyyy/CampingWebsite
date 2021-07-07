5a9b011c5e993a3edb27bff597e4899eda1f53e0
Sonali Suresh Shetty <61132919+Shettyyy@users.noreply.github.com>
2021-07-07 15:07:51 +0530

    added fancy clustermap

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const sample = array => array[Math.floor(Math.random()*array.length)];
 const seedDB = async ()=>{
     await Campground.deleteMany({});
     await Review.deleteMany({});
-    for(let i=0;i<50;i++){    
+    for(let i=0;i<300;i++){    
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -30,6 +30,10 @@ const seedDB = async ()=>{
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur nostrum commodi quas veritatis, odit quam molestiae perspiciatis tenetur illum? Voluptates delectus, nihil saepe deserunt sequi mollitia vel quasi corrupti odit!",
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [cities[random1000].longitude, cities[random1000].latitude]
+            },
             images: [
                 {
                     url : "https://res.cloudinary.com/dilsqlwhn/image/upload/v1625579095/CampWithCare/zowop1spmuscqers74ld.jpg",
